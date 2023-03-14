646a3521f3f9dcd7a98611b3aaf66df8e8f75141
parkchaewon18 <0101angela@naver.com>
2023-03-14 11:51:31 +0900

    increase

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -9,6 +9,11 @@ import org.springframework.web.bind.annotation.ResponseBody;
 // 아래 있는 HomeController 는 컨트롤러이다.
 @Controller
 public class HomeController {
+    private int count;
+
+    public HomeController() {
+        count = -1;
+    }
     // @GetMapping("/home/main") 의 의미
     // 개발자가 스프링부트에게 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
@@ -32,5 +37,10 @@ public class HomeController {
         return "즐거웠습니다.";
     }
 
-
+    @GetMapping("/home/increase")
+    @ResponseBody
+    public int showincrease() { // 리턴되는 int 값은 String 화 되어서 고객(브라우저)에게 전달된다.
+        count++;
+        return count;
+    }
 }
