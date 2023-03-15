eb4e4550988d880140f821b0a7620ba0b980b57e
parkchaewon18 <0101angela@naver.com>
2023-03-15 19:13:50 +0900

    사람추가

--- src/main/java/com/ll/basic1/HomeController.java
+++ src/main/java/com/ll/basic1/HomeController.java
@@ -1,9 +1,18 @@
 package com.ll.basic1;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.ArrayList;
+import java.util.List;
+
 
 // @Controller 의 의미
 // 개발자가 스프링부트에게 말한다.
@@ -11,10 +20,13 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
     private int count;
+    private List<Person> people;
 
     public HomeController() {
         count = -1;
+        people = new ArrayList<>();
     }
+
     // @GetMapping("/home/main") 의 의미
     // 개발자가 스프링부트에게 말한다.
     // 만약에 /home/main 이런 요청이 오면 아래 메서드를 실행해줘
@@ -47,9 +59,46 @@ public class HomeController {
 
     @GetMapping("/home/plus")
     @ResponseBody
-    //int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
-    //@RequestParam 는 생략가능
-    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam int b) {
+    // int a 는 쿼리스트링에서 a 파라미터의 값을 얻은 후 정수화 한 값이어야 한다.
+    // @RequestParam 는 생략가능
+    public int showPlus(@RequestParam(defaultValue = "0") int a, @RequestParam(defaultValue = "0") int b) {
         return a + b;
     }
+
+    @GetMapping("/home/addPerson")
+    @ResponseBody
+    public String addPerson(String name, int age) {
+        Person p = new Person(name, age);
+
+        System.out.println(p);
+
+        people.add(p);
+
+        return "%d번 사람이 추가되었습니다.".formatted(p.getId());
+    }
+
+    @GetMapping("/home/people")
+    @ResponseBody
+    public List<Person> showPeople() {
+        return people;
+    }
+}
+
+    @AllArgsConstructor
+    @Getter
+    @ToString
+    class Person {
+        private static int lastId;
+        private final int id;
+        private final String name;
+        private final int age;
+
+
+        static {
+            lastId = 0;
+        }
+
+        Person(String name, int age) {
+            this(++lastId, name, age);
+        }
 }
